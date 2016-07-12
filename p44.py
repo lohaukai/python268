c7f7f7178cf0a88e9d162805fe3e74582a85d03a
lohaukai <lohaukai@gmail.com>
2016-07-12 13:18:20 +0800

    edit p44.py for judge
    change len(str) compare to str compare

--- p44.py
+++ p44.py
@@ -1,8 +1,8 @@
 strIn1 = input()
 strIn2 = input()
-if len(strIn1) < len(strIn2):
+if strIn1 < strIn2:
     print('1<2')
-elif len(strIn1) > len(strIn2):
+elif strIn1 > strIn2:
     print('1>2')
 else:
     print('1==2')
